d544686ef04b12596350a65e8a76c4e79f567d18
Denis Lafontant <icebox827@gmail.com>
2021-04-27 11:14:59 -0400

    Add propTypes to display function

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Display({result}) {
-  
-}
+function Display(props) {
+  const { result } = props;
+  return <h1 className="display">{result}</h1>;
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+Display.defaultProps = {
+  result: '0',
+};
 
 export default Display;
