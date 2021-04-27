da597ce7b9883c7a74b6004d136bcd48de2cca8d
Denis Lafontant <icebox827@gmail.com>
2021-04-27 10:42:51 -0400

    Remove unsed files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../App.css';
-import React from 'react';
+
 
 function App() {
   return (
     <React.Fragment>
-      <h1>Hello world</h1>
+      <h1>Calculator</h1>
       <Display />
       <ButtonPanel />
     </React.Fragment>
