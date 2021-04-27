3b292f3f884b18f3a7e954583ef258b6d30e363f
Denis Lafontant <icebox827@gmail.com>
2021-04-27 16:02:19 -0400

    Create calculate function

---
+++ src/logic/calculate.js
@@ -0,0 +1,15 @@
+function Calculate(calcData, btnName) {
+  let { total, next, operation } = calcData;
+
+  if (btnName === 'AC') {
+    total = '';
+    next = '';
+    operation = ''
+    return calcData;
+  }
+
+  if (btnName === )
+
+};
+
+export default Calculate;
