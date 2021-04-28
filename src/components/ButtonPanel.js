a00338bf06a3062e37a010d0e9526c64ea81580c
Denis Lafontant <icebox827@gmail.com>
2021-04-27 22:18:42 -0400

    Update ButtonPanel with event listener

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -3,36 +3,40 @@ import PropTypes from 'prop-types';
 import Button from './Button';
 
 function ButtonPanel() {
+  // eslint-disable-next-line react/prop-types
+  const { clickHandler } = props;
+  
+
   return (
     <div className="btnPannel">
       <div className="group1">
-        <Button name="AC" />
-        <Button name="+/-" />
-        <Button name="%" />
-        <Button name="/" />
+        <Button name="AC" onClick={clickHandler} />
+        <Button name="+/-" onClick={clickHandler} />
+        <Button name="%" onClick={clickHandler} />
+        <Button name="/" onClick={clickHandler} />
       </div>
       <div className="group2">
-        <Button name="7" />
-        <Button name="8" />
-        <Button name="9" />
-        <Button name="x" />
+        <Button name="7" onClick={clickHandler} />
+        <Button name="8" onClick={clickHandler} />
+        <Button name="9" onClick={clickHandler} />
+        <Button name="x" onClick={clickHandler}/>
       </div>
       <div className="group3">
-        <Button name="4" />
-        <Button name="5" />
-        <Button name="6" />
-        <Button name="-" />
+        <Button name="4" onClick={clickHandler}/>
+        <Button name="5" onClick={clickHandler}/>
+        <Button name="6" onClick={clickHandler}/>
+        <Button name="-" onClick={clickHandler}/>
       </div>
       <div className="group4">
-        <Button name="1" />
-        <Button name="2" />
-        <Button name="3" />
-        <Button name="+" />
+        <Button name="1" onClick={clickHandler}/>
+        <Button name="2" onClick={clickHandler}/>
+        <Button name="3" onClick={clickHandler}/>
+        <Button name="+" onClick={clickHandler}/>
       </div>
       <div className="group5">
-        <Button name="0" />
-        <Button name="." />
-        <Button name="=" />
+        <Button name="0" onClick={clickHandler}/>
+        <Button name="." onClick={clickHandler}/>
+        <Button name="=" onClick={clickHandler}/>
       </div>
     </div>
   )
