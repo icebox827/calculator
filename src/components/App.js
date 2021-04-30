38abe05ead5053c90010de0a5606bea9702f03fc
Denis Lafontant <icebox827@gmail.com>
2021-04-30 16:30:20 -0400

    Use hook in App function

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import calculate from '../logic/calculate';
 import '../App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick = btnName => {
-    const data = { ...this.state };
+const App = () => {
+  const [value, setValue] = useState ({
+          total: null,
+          next: null,
+          operation: null,
+  });
+ 
+  const handleClick = btnName => {
+    const data = { ...value };
     const calculation = calculate(data, btnName);
-    this.setState({ ...calculation });
+    setValue({ ...calculation });
   }
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div>
-        <h1>React Calculator</h1>
-        <Display result={(next && next.toString()) || (total && total.toString())} />
-        <ButtonPanel clickHandler={data => this.handleClick(data)} />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>React Calculator</h1>
+      <Display result={value.operation ? value.next : value.total} />
+      <ButtonPanel clickHandler={data => handleClick(data)} />
+    </div>
+  );
 }
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//     };
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+
+//   handleClick = btnName => {
+//     const data = { ...this.state };
+//     const calculation = calculate(data, btnName);
+//     this.setState({ ...calculation });
+//   }
+
+//   render() {
+//     const { next, total } = this.state;
+//     return (
+//       <div>
+//         <h1>React Calculator</h1>
+//         <Display result={(next && next.toString()) || (total && total.toString())} />
+//         <ButtonPanel clickHandler={data => this.handleClick(data)} />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
