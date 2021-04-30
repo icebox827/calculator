925a8bbb91b025a14e687e92a70926a61ad47635
Denis Lafontant <icebox827@gmail.com>
2021-04-30 18:30:57 -0400

    Import app.css

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
+import '../App.css';
 
 function ButtonPanel(props) {
   const { clickHandler } = props;
