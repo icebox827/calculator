e1d78699244b74b30741a4c85b4852ea50bf3280
Denis Lafontant <icebox827@gmail.com>
2021-05-01 13:41:18 -0400

    Create operation test

---
+++ src/test/calculate.test.js
@@ -0,0 +1,17 @@
+const calculate = require('../logic/calculate');
+
+it('should add number', () => {
+  expect(2 + 2).toBe(4);
+});
+
+it('should multiply number', () => {
+  expect(2 * 2).toBe(4);
+});
+
+it('should substract number', () => {
+  expect(6 - 2).toBe(4);
+});
+
+it('should divide number', () => {
+  expect(8 / 2).toBe(4);
+});
